cf71971bed565b86f5db604de622f18f46decdd6
PUTLURU NAGALAKSHMI <nagalakshmiputluru2@gmail.com>
2026-04-18 19:44:10 +0530

    digital clock project upload

---
+++ digital_clock.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import time
+
+# create window
+root = Tk()
+root.title("Digital Clock")
+root.geometry("400x150")
+root.resizable(False, False)
+
+# function to update time
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
+
+# label to show time
+clock_label = Label(
+    root,
+    font=("Arial", 50),
+    background="black",
+    foreground="cyan"
+)
+clock_label.pack(expand=True)
+
+update_time()
+root.mainloop()
